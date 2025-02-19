78339c8494fdf48e3a6a2727b45167fd40645171
Zomeiro <ba.vangen5@gmail.com>
2025-02-19 23:07:02 +0100

    Added class for wrapping lines w/ an opening and end. extends ReplaceTextCommand

---
+++ Oving3/src/main/java/com/bjorav/ovinger/commands/WrapLinesTextCommand.java
@@ -0,0 +1,20 @@
+package com.bjorav.ovinger.commands;
+
+public class WrapLinesTextCommand extends WrapTextCommand {
+
+  WrapLinesTextCommand(String opening, String end) {
+    super(opening, end);
+  }
+
+  @Override
+  public String execute(String text) {
+    String[] lines = text.split("\n");
+    StringBuilder result = new StringBuilder();
+
+    for (String line : lines) {
+      result.append(getOpening()).append(line).append(getEnd()).append("\n");
+    }
+
+    return result.toString();
+  }
+}
