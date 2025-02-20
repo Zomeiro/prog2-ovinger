7c1b9fcacd09f8ba24854624e8609f4810879e70
Zomeiro <ba.vangen5@gmail.com>
2025-02-20 02:56:15 +0100

    Added class to capitalize first chr of string. implements textcommand

---
+++ Oving3/src/main/java/com/bjorav/ovinger/commands/CapitalizeTextCommand.java
@@ -0,0 +1,12 @@
+package com.bjorav.ovinger.commands;
+
+public class CapitalizeTextCommand implements TextCommand {
+
+  @Override
+  public String execute(String text) {
+    if (text == null || text.isEmpty()) {
+      return text;
+    }
+    return text.substring(0, 1).toUpperCase() + text.substring(1);
+  }
+}
